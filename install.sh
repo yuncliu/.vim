f39bffa0d1fbc20fa224b7b03e24cd6d794f8dfe
yuncliu <lyc1990@qq.com>
2016-01-14 17:49:15 +0800

    add generate stl tags automaticly

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
+echo "Hellow"
 if [ -d ~/.vim/bundle/Vundle.vim ]; then
     echo "vundle is already installed"
 else
     git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 vim +PluginInstall +qall
+
+if [ -f ~/.vim/stl_tags ]; then
+    echo 'stl tags exist'
+else
+    echo 'creating stl tags'
+    bash stl.sh
+fi
--- stl.sh
+++ stl.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
+for v in `ls /usr/include/c++`
+do
+    break
+done
+p='/usr/include/c++/'$v
 
-
-if [ $# != 1 ]; then
-    p='/usr/include/c++/4.8'
-else
-    p=$1
-fi
-echo $p
 cp -r $p cpp_src
 find ./cpp_src -type f | xargs sed -i 's/namespace std _GLIBCXX_VISIBILITY(default)/namespace std/'
 ctags -f stl_tags -R --c++-kinds=+p --fields=+iaS --extra=+q --language-force=C++ -I _GLIBCXX_NOEXCEPT cpp_src
